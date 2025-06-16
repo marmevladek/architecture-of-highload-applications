269b50f3ec0e7c1fafbf60f0cae1986cee3b7cfb
Vladislav <vladislav.tezev@gmail.com>
2025-06-17 02:18:20 +0300

    Add retryaer
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

--- deck-service/src/main/java/ru/dating/app/deckservice/external/client/ProfileServiceClient.java
+++ deck-service/src/main/java/ru/dating/app/deckservice/external/client/ProfileServiceClient.java
@@ -3,13 +3,15 @@ package ru.dating.app.deckservice.external.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.dating.app.deckservice.config.FeignRetryConfig;
 import ru.dating.app.deckservice.payload.ProfileResponse;
 
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "profile-service", url = "${profile-service.url}")
+@FeignClient(name = "profile-service", url = "${profile-service.url}", configuration = FeignRetryConfig.class)
 public interface ProfileServiceClient {
+
     @GetMapping("/api/profiles")
     List<ProfileResponse> getProfiles(@RequestParam("excludeUserId") UUID excludeUserId);
 
